638e0c02929955f2f7c4c1227a09eb1ec2920195
mr-valentos <nerpavlad@gmail.com>
2022-05-23 21:34:49 +0300

    component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
 import "./style.scss"
+
 console.log("Hello webpack!")
 
 const fancyFunc = () => {
     return [1, 2];
   };
   
-  const [a, b] = fancyFunc();
+const [a, b] = fancyFunc();
+
+function App() {
+    const [state, setState] = useState("CLICK ME");
+    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+}
+
+render(<App />, document.getElementById("root"));
